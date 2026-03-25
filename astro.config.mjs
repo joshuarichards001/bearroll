c92966918e5d52d9f9cd5c31c2ebc69bfb9a5b09
Josh Richards <joshuarichards001@gmail.com>
2026-03-25 19:50:40 +0000

    Update url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  site: "https://bear-blog-website.github.io",
+  site: "https://joshuarichards001.github.io",
+  base: "/bear-blog-website",
 });
