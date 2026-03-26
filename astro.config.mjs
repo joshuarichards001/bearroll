ac9b584f03d9aba3797cd7dffd5023d951afd934
Josh Richards <joshuarichards001@gmail.com>
2026-03-26 00:04:32 +0000

    SEO Improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   output: "static",
   site: "https://bearroll.dev",
   base: "/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/api/"),
+    }),
+  ],
 });
